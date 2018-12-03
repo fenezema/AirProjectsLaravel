8cfb268f8622397c51acb7b92cc4e5884a904256
Andrean Januar Priatmojo <an.jp29@gmail.com>
2018-12-03 21:52:34 +0700

    add myproject

---
+++ resources/views/pages/myproject.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.apApp')
+@section('content')
+<section id="services">
+	<div class="container">
+		<div class="section-header" style="margin-bottom: 3em;">
+			<h2>My Projects</h2>
+		</div>
+		<div class="row">
+			<div class="col-md-10">
+				<div class="box wow fadeInRight">
+	                <h4 class="title">build me a website like tophatter.com</h4>
+	                <p class="description"><b>[P Baskara]</b></p>
+	                <p class="description" style="font-size: 20px;margin-top: 1em;">
+	                	<span class="badge badge-pill badge-success" style="margin-right: 1em;">Rp. 126.000.000</span>
+	                	<span class="badge badge-pill badge-danger" style="margin-right: 1em;">3 bulan</span>
+	                	<button class="btn btn-primary" type="button" style="margin-left: 20em">ini tombol</button>
+	                </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection

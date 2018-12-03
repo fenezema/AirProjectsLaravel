979f257cf386e9d703819141ab414e79b159b5cf
Andrean Januar Priatmojo <an.jp29@gmail.com>
2018-12-03 22:25:34 +0700

    Create membership.blade.php

---
+++ resources/views/pages/membership.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.apApp')
+@section('content')
+<section id="services">
+	<div class="container">
+		<div class="section-header" style="margin-bottom: 3em;">
+			<h2>Come, join us</h2>
+		</div>
+		<div>
+			<div class="table-responsive">
+				<table class="table table-borderless">
+					<thead>
+						<tr>
+							<th><center><h1><b style="color: #0065FF">for Worker</b></h1></center></th>
+							<th>
+								<center>
+									<p style="color: #FFBB00">Premium</p>
+									<h2>Rp. 50.000</h2>
+								</center>
+							</th>
+							<th>
+								<center>
+									<p style="color: #FFBB00">Free</p>
+									<h2>Rp. 0</h2>
+								</center>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><center>Project Limit</center></td>
+							<td><center>2</center></td>
+							<td><center>5</center></td>
+						</tr>
+						<tr>
+							<td><center>Special Account *</center></td>
+							<td><center>YES</center></td>
+							<td><center>NO</center></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>
+								<center>
+									<button type="button" class="btn btn-dark">SUBSCRIBE</button>
+								</center>
+							</td>
+							<td>
+								<center>
+									<button type="button" class="btn btn-dark">SUBSCRIBE</button>
+								</center>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<br><br><br><br>
+				<table class="table table-borderless">
+					<thead>
+						<tr>
+							<th><center><h1><b style="color: #0065FF">for Project Owner</b></h1></center></th>
+							<th>
+								<center>
+									<p style="color: #FFBB00">Premium</p>
+									<h2>Rp. 100.000</h2>
+								</center>
+							</th>
+							<th>
+								<center>
+									<p style="color: #FFBB00">Free</p>
+									<h2>Rp. 0</h2>
+								</center>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><center>Project Limit</center></td>
+							<td><center>3</center></td>
+							<td><center>7</center></td>
+						</tr>
+						<tr>
+							<td><center>Special Account *</center></td>
+							<td><center>YES</center></td>
+							<td><center>NO</center></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>
+								<center>
+									<button type="button" class="btn btn-dark">SUBSCRIBE</button>
+								</center>
+							</td>
+							<td>
+								<center>
+									<button type="button" class="btn btn-dark">SUBSCRIBE</button>
+								</center>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<br><br><br>
+				<div>
+					<p>*) Members will always be placed first, before regulars. Get more people to contact you easily</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
